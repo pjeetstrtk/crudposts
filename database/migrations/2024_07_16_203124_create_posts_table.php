be04cb05905f40975fd08d41901344c498883f81
pjeetstrtk <pjeetstrtk@gmail.com>
2024-07-18 02:30:47 +0600

    complete

--- database/migrations/2024_07_16_203124_create_posts_table.php
+++ database/migrations/2024_07_16_203124_create_posts_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('title');
             $table->text('body');
             $table->timestamps();
-          });
+        });
     }
 
     /**
